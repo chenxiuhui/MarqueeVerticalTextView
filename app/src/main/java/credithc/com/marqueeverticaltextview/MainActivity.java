4793eef4c3b12ad127a41552e5c7da21eb0206ee
chenxiuhui <chen@qq.com>
2016-07-31 22:48:47 +0800

    Initial commit

---
+++ app/src/main/java/credithc/com/marqueeverticaltextview/MainActivity.java
@@ -0,0 +1,33 @@
+package credithc.com.marqueeverticaltextview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MarqueeView marqueeView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        marqueeView = (MarqueeView) findViewById(R.id.marqueeView);
+
+        List<String> info = new ArrayList<>();
+        info.add("1. 大家好，我是陈秀辉。");
+        info.add("2. 欢迎大家关注我哦！");
+        info.add("3. 我们都一样");
+        info.add("4. 哈哈哈哈哈哈哈哈");
+        info.add("5. 走过岁月我才发现");
+        info.add("6. 好的好好好的");
+        marqueeView.startWithList(info);
+    }
+}
