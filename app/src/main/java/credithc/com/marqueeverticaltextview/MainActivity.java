c4575d587f56b9b6136304786d22556d171e5f25
chenxiuhui <m13552888619@163.com>
2016-12-10 16:20:49 +0800

    新代码

--- app/src/main/java/credithc/com/marqueeverticaltextview/MainActivity.java
+++ app/src/main/java/credithc/com/marqueeverticaltextview/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         info.add("7. test git branch");
         info.add("8. test git branch222");
         info.add("9. test git branch333");
+        info.add("10. test git branch4444444");
         marqueeView.startWithList(info);
     }
 }
